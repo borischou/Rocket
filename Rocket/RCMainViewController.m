1111efa6236b53eb87e10f405d15bff2ad03cc7f
Boris <zhouboli@gmail.com>
2015-07-23 18:12:04 +0800

    2nd ci

--- Rocket/RCMainViewController.m
+++ Rocket/RCMainViewController.m
@@ -6,8 +6,245 @@
 //  Copyright (c) 2015年 Bankwel. All rights reserved.
 //
 
+#import <MAMapKit/MAMapKit.h>
+#import "UberKit.h"
+
 #import "RCMainViewController.h"
+#import "RCBottomMenuView.h"
+#import "RCCenterPinView.h"
+#import "RCCarTypeCollectionView.h"
+#import "RCCarTypeCollectionViewCell.h"
+#import "RCPaopaoView.h"
+#import "RCFocusView.h"
+
+#define bWidth [UIScreen mainScreen].bounds.size.width
+#define bHeight [UIScreen mainScreen].bounds.size.height
+
+#define bMenuHeight bHeight/5
+#define bScaleBarHeight 30
+#define bFocusBtnHeight 40
+#define bPaopaoViewHeight 40
+
+static NSString *gaodeMapAPIKey = @"9f692108300515ec3819e362d6389159";
+
+@interface RCMainViewController () <UICollectionViewDataSource, UICollectionViewDelegate, MAMapViewDelegate>
+
+@property (copy, nonatomic) NSString *uberWaitingMins;
+@property (copy, nonatomic) NSString *curAddress;
+@property (nonatomic) CGPoint paopaoCenter;
+
+@property (strong, nonatomic) RCPaopaoView *paopaoView;
+@property (strong, nonatomic) RCFocusView *focusView;
+@property (strong, nonatomic) RCBottomMenuView *menuView;
+@property (strong, nonatomic) RCCenterPinView *centerPinView;
+@property (strong, nonatomic) RCCarTypeCollectionView *carTypeCollectionView;
+
+@property (strong, nonatomic) MAMapView *mapView;
+@property (strong, nonatomic) MAPinAnnotationView *curPinView;
+@property (strong, nonatomic) MAPointAnnotation *curAnnotation;
+
+@property (strong, nonatomic) UberTime *estimateTime;
+@property (strong, nonatomic) UberProfile *profile;
+
+@property (nonatomic) BOOL isInitLoad;
+@property (nonatomic) BOOL isCentered;
+
+@end
 
 @implementation RCMainViewController
+
+#pragma mark - View Controller Life Cycle
+
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self loadMenuView];
+    [self loadBarbuttonItems];
+    [self loadGaodeMapView];
+    [self loadCollectionView];
+}
+
+#pragma mark - Load View
+
+-(void)loadMenuView
+{
+    _isInitLoad = YES;
+    _menuView = [[RCBottomMenuView alloc] init];
+    _menuView.userInteractionEnabled = YES;
+    [self.view addSubview:_menuView];
+    
+    [_menuView.destLbl addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapDestinationLabel:)]];
+    [_menuView.requestBtn addTarget:self action:@selector(requestButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [_menuView.compareBtn addTarget:self action:@selector(compareButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+}
+
+-(void)loadFloatViews
+{
+    _centerPinView = [[RCCenterPinView alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
+    _centerPinView.center = CGPointMake(_mapView.center.x, _mapView.center.y-25);
+    [self.view addSubview:_centerPinView];
+    
+    _paopaoView = [[RCPaopaoView alloc] initWithFrame:CGRectMake(0, 0, 10, 10)];
+    _paopaoCenter = CGPointMake(_centerPinView.center.x, _centerPinView.center.y - 35);
+    _paopaoView.center = _paopaoCenter;
+    _paopaoView.addrLbl.text = _curAddress;
+    [_paopaoView.addrLbl addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapLabel:)]];
+    [self.view addSubview:_paopaoView];
+    
+    _focusView = [[RCFocusView alloc] initWithFrame:CGRectMake(10, bHeight - bMenuHeight - bScaleBarHeight - 10 - bFocusBtnHeight, 40, bFocusBtnHeight)];
+    [_focusView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapFocus:)]];
+    [self.view addSubview:_focusView];
+}
+
+-(void)loadBarbuttonItems
+{
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 23, 23)];
+    [imageView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(profileBarButtonPressed)]];
+    imageView.userInteractionEnabled = YES;
+    imageView.image = [UIImage imageNamed:@"hk_profile_4"];
+    UIBarButtonItem *profileBarbutton = [[UIBarButtonItem alloc] initWithCustomView:imageView];
+    UIImageView *settingsView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 23, 23)];
+    [settingsView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(settingsBarbuttonPressed)]];
+    settingsView.userInteractionEnabled = YES;
+    settingsView.image = [UIImage imageNamed:@"hk_settings"];
+    UIBarButtonItem *settingsBarbutton = [[UIBarButtonItem alloc] initWithCustomView:settingsView];
+    
+    self.navigationItem.rightBarButtonItem = settingsBarbutton;
+    self.navigationItem.leftBarButtonItem = profileBarbutton;
+}
+
+-(void)loadCollectionView
+{
+    _carTypeCollectionView = [[RCCarTypeCollectionView alloc] initWithFrame:CGRectMake(0, bHeight - bMenuHeight, bWidth, bMenuHeight/2) collectionViewLayout:[[UICollectionViewFlowLayout alloc] init]];
+    [_carTypeCollectionView registerClass:[RCCarTypeCollectionViewCell class] forCellWithReuseIdentifier:@"reuseCell"];
+    _carTypeCollectionView.dataSource = self;
+    _carTypeCollectionView.delegate = self;
+    [self.view addSubview:_carTypeCollectionView];
+}
+
+-(void)loadGaodeMapView
+{
+    [MAMapServices sharedServices].apiKey = gaodeMapAPIKey;
+    _mapView = [[MAMapView alloc] initWithFrame:CGRectMake(0, 0, bWidth, bHeight-bMenuHeight)];
+    _mapView.delegate = self;
+    [self.view addSubview:_mapView];
+}
+
+#pragma mark - UIButtons
+
+-(void)requestButtonPressed:(UIButton *)sender
+{
+
+}
+
+-(void)compareButtonPressed:(UIButton *)sender
+{
+    
+}
+
+-(void)profileBarButtonPressed
+{
+    
+}
+
+-(void)settingsBarbuttonPressed
+{
+    
+}
+
+#pragma mark - Tap Actions
+
+-(void)tapDestinationLabel:(UITapGestureRecognizer *)tap
+{
+
+}
+
+-(void)tapLabel:(UITapGestureRecognizer *)tap
+{
+
+}
+
+-(void)tapFocus:(UITapGestureRecognizer *)tap
+{
+
+}
+
+#pragma mark - Helpers
+
+-(NSAttributedString *)attributedStringForBrandLabel:(NSString *)string
+{
+    NSAttributedString *aString = [[NSAttributedString alloc] initWithString:string attributes:@{NSFontAttributeName: [UIFont systemFontOfSize:13.f]}];
+    return aString;
+}
+
+#pragma mark - UICollectionViewDataSource
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    RCCarTypeCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"reuseCell" forIndexPath:indexPath];
+    
+    switch (indexPath.row) {
+        case 0: //Uber
+            cell.brandTextLabel.attributedText = [self attributedStringForBrandLabel:@"优步"];
+            cell.brandIconView.image = [UIImage imageNamed:@"hk_uber_icon"];
+            if (_uberWaitingMins) {
+                cell.waitingTimeLabel.attributedText = [[NSAttributedString alloc] initWithString:_uberWaitingMins attributes:@{NSFontAttributeName: [UIFont systemFontOfSize:10.f]}];
+            } else {
+                cell.waitingTimeLabel.text = @"";
+            }
+            break;
+        case 1: //滴滴打车
+            cell.brandTextLabel.attributedText = [self attributedStringForBrandLabel:@"滴滴打车"];
+            cell.brandIconView.image = [UIImage imageNamed:@"hk_didi_icon"];
+            cell.waitingTimeLabel.text = @"";
+            break;
+        case 2:
+            cell.brandTextLabel.attributedText = [self attributedStringForBrandLabel:@"快的打车"];
+            cell.brandIconView.image = [UIImage imageNamed:@"hk_kuaidi_icon"];
+            cell.waitingTimeLabel.text = @"";
+            break;
+        case 3:
+            cell.brandTextLabel.attributedText = [self attributedStringForBrandLabel:@"神州专车"];
+            cell.brandIconView.image = [UIImage imageNamed:@"hk_shenzhou_icon"];
+            cell.waitingTimeLabel.text = @"";
+            break;
+        case 4:
+            cell.brandTextLabel.attributedText = [self attributedStringForBrandLabel:@"51用车"];
+            cell.brandIconView.image = [UIImage imageNamed:@"hk_51_icon"];
+            cell.waitingTimeLabel.text = @"";
+            break;
+            
+        default:
+            break;
+    }
+    
+    return cell;
+}
+
+#pragma mark - UICollectionViewDelegate
+
+-(void)collectionView:(UICollectionView *)collectionView willDisplayCell:(UICollectionViewCell *)cell forItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (!_isCentered) {
+        cell.contentView.transform = CGAffineTransformMakeScale(0.5, 0.5);
+        [UIView beginAnimations:nil context:UIGraphicsGetCurrentContext()];
+        [UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
+        [UIView setAnimationDuration:2.0];
+        cell.contentView.transform = CGAffineTransformMakeScale(1.0f, 1.0f);
+        [UIView commitAnimations];
+    }
+}
+
+
 
 @end
